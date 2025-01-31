5a1f2dd61cb7c9fa4bbf26a7c4ddc2d95e9d9beb
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-01-31 13:49:41 +0100

    setting bytes directly to pixel

--- core/io/image.cpp
+++ core/io/image.cpp
@@ -3299,6 +3299,52 @@ void Image::set_pixel(int p_x, int p_y, const Color &p_color) {
 	_set_color_at_ofs(data.ptrw(), ofs, p_color);
 }
 
+void Image::set_pixel_bytes(const int x, const int y, const PackedByteArray &bytes){
+#ifdef DEBUG_ENABLED
+	ERR_FAIL_INDEX(x, width);
+	ERR_FAIL_INDEX(y, height);
+#endif
+	int32_t expected_byte_count = -1;
+	switch (format) {
+		case FORMAT_R8:
+			expected_byte_count = 1;
+			break;
+		case FORMAT_RG8:
+			expected_byte_count = 2;
+			break;
+		case FORMAT_RGB8:
+			expected_byte_count = 3;
+			break;
+		case FORMAT_RGBA8:
+			expected_byte_count = 4;
+			break;
+		case FORMAT_RGBA4444:
+			expected_byte_count = 2;
+			break;
+		case FORMAT_RF:
+			expected_byte_count = 4;
+			break;
+		case FORMAT_RGF:
+			expected_byte_count = 8;
+			break;
+		case FORMAT_RGBF:
+			expected_byte_count = 12;
+			break;
+		default:
+			ERR_FAIL_MSG("Unsupported Image format.");
+			break;
+	}
+	if (expected_byte_count != bytes.size()){
+		ERR_FAIL_MSG("Passed bytes are not the right length for a pixel.");
+		return;
+	}
+	uint32_t offset = y * width + x;
+	uint8_t *data_ptr = data.ptrw();
+	for (int i = 0; i < bytes.size(); i++){
+		data_ptr[offset + i] = bytes[i];
+	}
+}
+
 void Image::adjust_bcs(float p_brightness, float p_contrast, float p_saturation) {
 	ERR_FAIL_COND_MSG(!_can_modify(format), "Cannot adjust_bcs in compressed or custom image formats.");
 
@@ -3483,6 +3529,7 @@ void Image::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_pixel", "x", "y"), &Image::get_pixel);
 	ClassDB::bind_method(D_METHOD("set_pixelv", "point", "color"), &Image::set_pixelv);
 	ClassDB::bind_method(D_METHOD("set_pixel", "x", "y", "color"), &Image::set_pixel);
+	ClassDB::bind_method(D_METHOD("set_pixel_bytes", "x", "y", "bytes"), &Image::set_pixel_bytes);
 
 	ClassDB::bind_method(D_METHOD("adjust_bcs", "brightness", "contrast", "saturation"), &Image::adjust_bcs);
 
--- core/io/image.h
+++ core/io/image.h
@@ -424,6 +424,7 @@ public:
 	Color get_pixel(int p_x, int p_y) const;
 	void set_pixelv(const Point2i &p_point, const Color &p_color);
 	void set_pixel(int p_x, int p_y, const Color &p_color);
+	void set_pixel_bytes(int x, int y, const PackedByteArray &bytes);
 
 	void adjust_bcs(float p_brightness, float p_contrast, float p_saturation);
 
